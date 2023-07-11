f517470e7995f999fa83e269a072ac00b5d94e03
xeno2911 <xenorevenge@gmail.com>
2023-07-11 01:06:31 +0100

    0x15 File I/O

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * read_textfile - Reads a text from a file and then prints it
+ * @filename: Name of the file to read
+ * @letters: Number of bytes to read
+ *
+ * Return: Number of bytes read/printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t bytes;
+	char buf[READ_BUF_SIZE * 8];
+
+	if (!filename || !letters)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	bytes = read(fd, &buf[0], letters);
+	bytes = write(STDOUT_FILENO, &buf[0], bytes);
+	close(fd);
+	return (bytes);
+}
+
